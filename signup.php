54618d633f7744bd08d8743290551a82f14b0494
Cam <cjordan@oakland.edu>
2021-03-03 03:18:32 -0500

    Signup specific changes

--- signup.php
+++ signup.php
@@ -14,8 +14,9 @@
         <?php include('Includes/header.php'); ?>
 
         <section>
-            <div class="centered container login-area">
+            <div class="container login-area">
                 <h1>Sign Up</h1>
+                <p>Please fill out this form to create an account</p>
                 <form id="signupForm" action="signupProcess.php" method="post">
                     <label>First Name:</label>
                     <input type="text" name="firstName" size="40" title="Enter your first name" required>
@@ -26,11 +27,11 @@
                     <br>
 
                     <label>Last Name:</label>
-                    <input type="text" name="lastName" size="40" title="Enter your last name">
+                    <input type="text" name="lastName" size="40" title="Enter your last name" required>
                     <br>
 
                     <label>Date of Birth:</label>
-                    <input type="date" name="dateOfBirth" title="Enter your date of birth">
+                    <input type="date" name="dateOfBirth" title="Enter your date of birth" required>
                     <br>
 
                     <label>Gender:</label>
@@ -39,28 +40,30 @@
                         <option>Female</option>
                         <option>Other</option>
                     </select>
+                    <br>
 
                     <label>Home Address:</label>
-                    <input type="text" name="homeAddress" size="40" title="Enter your home address">
+                    <input type="text" name="homeAddress" size="40" title="Enter your home address" required>
                     <br>
 
                     <label>Email Address:</label>
-                    <input type="text" name="email" size="40" title="Enter your email address">
+                    <input type="text" name="email" size="40" title="Enter your email address" required>
                     <br>
 
                     <label>Username:</label>
-                    <input type="text" name="email" size="40" title="Create your username">
+                    <input type="text" name="email" size="40" title="Create your username" required>
                     <br>
 
                     <label>Password:</label>
-                    <input type="password" name="password1" size="40" title="Create your password">
+                    <input type="password" name="password1" size="40" title="Create your password" required>
                     <br>
 
                     <label>Re-enter Password:</label>
-                    <input type="password" name="password2" size="40" title="Re-enter your created password">
+                    <input type="password" name="password2" size="40" title="Re-enter your created password" required>
                     <br>
 
-                    <input type="submit" name="loginSubmit" value="Submit" class="fancyButton-1">
+                    <input type="submit" title="Click here to submit the entered data" name="loginSubmit" value="Submit" class="fancyButton-1">
+                    <input type="reset" title="Click here to reset all fields in the form" class="fancyButton-2" value="Reset"> 
                 </form>
             </div>
         </section>
